8bf4f274ae2f1f55a81807ff7c7bab7a78363037
Pengfei Zheng <pengfeizheng1@gmail.com>
2017-04-19 16:41:51 +0800

    Add footer

--- client/pages/Footer/index.js
+++ client/pages/Footer/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const Footer = () => (
-  <div>
-    <footer className="footer container text-center">
-      <p className="pull-left">© 2016 Company, Inc. All Rights Reserved</p>
-      <ul className="pull-right list-inline" >
-        <li><a href="https://github.com/balabala/universal-react-app">GitHub Project</a></li>
-        <li><a href="https://github.com/balabala/universal-react-app/issues">Issues</a></li>
-      </ul>
-    </footer>
-  </div>
+  <footer className="footer container text-center">
+    <hr />
+    <div class="container">
+      <a href="#">Frequently Asked Questions</a> | <a href="#">Terms of Service</a>
+      <p>© 2017 Company, Inc. All Rights Reserved</p>
+    </div>
+  </footer>
 );
 
 export default Footer;
